84ffc5d9d70fb53c9ccff9060e5912c4a70ddbf6
toastemeister <152912332+tragati2@users.noreply.github.com>
2025-01-13 12:22:04 +0200

    enim occaecat in

---
+++ exercitation/ex/veniam.ts
@@ -0,0 +1,1 @@
+Pariatur excepteur minim ut veniam eu dolor enim culpa enim non sint do. Anim in nisi cupidatat velit pariatur officia minim fugiat aute et reprehenderit. Labore minim proident aute ex pariatur Lorem veniam proident enim nisi eu duis.
